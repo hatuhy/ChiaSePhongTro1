e3a1a9006abb3dc15e60a3c27d8918cd216b2568
HaTu <hatu1998hy@gmail.com>
2019-09-26 08:56:32 +0700

    vvvv

---
+++ Demo/app/Http/Controllers/page.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+class page extends Controller
+{
+    //
+    public function getdangnhap()
+    {
+    	return view('login');
+    }
+    public function postdangnhap(Request $request)
+    {
+    	if(Auth::attempt(['email'=>$request->username,'password'=>$request->pass]))
+    		return redirect('index'); 
+    }
+}
---
+++ Demo/resources/views/thanhcong.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Dang nhap thanh cong</h1>
+</body>
+</html>
